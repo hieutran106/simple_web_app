472d3193fd951af7c311611c3744e092ce66d7b4
Trung Hieu Tran <trunghieu.tran@connect.qut.edu.au>
2016-06-24 13:14:59 +1000

    edit mysql connect

--- src/org/conn/MySQLConnUtils.java
+++ src/org/conn/MySQLConnUtils.java
@@ -18,6 +18,16 @@ public class MySQLConnUtils {
 	public static Connection getMySQLConnection(String hostName, String dbName,
 	        String userName, String password) throws SQLException,
 	        ClassNotFoundException {
+		String driver = "com.mysql.jdbc.Driver";
+		try {
+			Class.forName(driver).newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		String connectionURL="jdbc:mysql://"+hostName+":3306/"+dbName;
 		Connection conn=DriverManager.getConnection(connectionURL, userName, password);
 		return conn;
